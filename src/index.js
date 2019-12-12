76d3946c000661fb3bece12741148a84e58917d9
Meg Gutshall <meghan.gutshall@gmail.com>
2019-12-12 17:14:35 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,53 +1,76 @@
-// Fetch all toys via GET request
 async function fetchToys() {
   const resp = await fetch("http://localhost:3000/toys");
   return data = await resp.json();
 };
 
-// Take the Response and make a div with class "card" for each toy
-async function saveToys() {
-  // Create toy collection element
+async function formSubmit() {
+  const submitBtn = await document.querySelector(".add-toy-form input")
+  submitBtn.addEventListener("click", saveNewToy);
+};
+
+// Save toy to database
+function saveNewToy() {
+  let formData = {
+    name: name,
+    image: image,
+    likes: 0
+  };
+
+  let obj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  // Fetch newly created toy
+  fetch("http://localhost:3000/toys", obj)
+    .then(resp => resp.json())
+    .then(toy => console.log(toy))
+    .catch(error => console.log(error.message));
+};
+
+// Render newly created toy
+
+async function displayToys() {
   let toyCollection = document.getElementById("toy-collection");
   const allToys = await fetchToys();
 
-  function displayToys(toys) {
-    toys.forEach(toy => {
+  function parseToys(toys) {
+
+    function buildCard(toy){
       const card = document.createElement("div")
       card.setAttribute("class", "card");
-        // Card elements should include:
-          // h2 with toy name
-          // img with src of the toy image attribute and the class name "toy-avatar"
-          // p with number of likes
-          // button with class "like-btn"
-        function buildCard(toy) {
-          let toyName = document.createElement("h2");
-          let toyImage = document.createElement("img");
-          let likes = document.createElement("p");
-          let likeBtn = document.createElement("button");
-          toyName.innerText = toy.name;
-          toyImage.setAttribute("class", "toy-avatar");
-          toyImage.src = toy.image;
-          likes.innerText = `${toy.likes} Likes`;
-          likeBtn.setAttribute("class", "like-btn");
-          likeBtn.innerText = `Like ${toy.name}`;
-          card.appendChild(toyName);
-          card.appendChild(toyImage);
-          card.appendChild(likes);
-          card.appendChild(likeBtn);
-        };
-        buildCard(toy);
-      // Append each card to toy collection element
+
+      let toyName = document.createElement("h2");
+      let toyImage = document.createElement("img");
+      let likes = document.createElement("p");
+      let likeBtn = document.createElement("button");
+      toyName.innerText = toy.name;
+      toyImage.setAttribute("class", "toy-avatar");
+      toyImage.src = toy.image;
+      likes.innerText = `${toy.likes} Likes`;
+      likeBtn.setAttribute("class", "like-btn");
+      likeBtn.innerText = `Like ${toy.name}`;
+      card.append(toyName, toyImage, likes, likeBtn);
+
       toyCollection.appendChild(card);
-    })
+    };
+
+    toys.forEach(toy => { buildCard(toy) });
   };
 
-  displayToys(allToys);
+  parseToys(allToys);
 };
 
 
 document.addEventListener("DOMContentLoaded", function() {
-  saveToys();
+  displayToys();
+  formSubmit();
 });
+
 
 let addToy = false
 
