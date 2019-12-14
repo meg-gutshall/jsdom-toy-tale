24baa4d0ea404bca03b8a3db952a053dcba24fbb
Meg Gutshall <meghan.gutshall@gmail.com>
2019-12-13 20:24:55 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
-async function fetchToys() {
-  const resp = await fetch("http://localhost:3000/toys");
-  return data = await resp.json();
+// Declare global variables
+const toyCollection = document.getElementById("toy-collection");
+let addToy = false
+
+// Define fetch that returns all toys after being fed into the card builder function
+function renderToys() {
+  return fetch("http://localhost:3000/toys")
+  .then(resp => resp.json())
+  .then(function(allToys) {
+    allToys.forEach(toy => { buildCard(toy) });
+  })
+  .catch(error => console.log(error.message));
 };
 
-async function formSubmit() {
-  const submitBtn = await document.querySelector(".add-toy-form input.submit")
-  submitBtn.addEventListener("click", saveNewToy);
-};
-
-// Save toy to database
-async function saveNewToy(name, image) {
+// Save new toy to database
+function saveNewToy(event_data) {
   let formData = {
-    name: name,
-    image: image,
+    name: event_data.name.value,
+    image: event_data.image.value,
     likes: 0
   };
-
+  
   let obj = {
     method: "POST",
     headers: {
@@ -24,62 +28,52 @@ async function saveNewToy(name, image) {
     },
     body: JSON.stringify(formData)
   };
-
-  // Fetch newly created toy
-  const resp = await fetch("http://localhost:3000/toys", obj);
-  return data = await resp.json();
+  
+  // Fetch and render newly created toy after feeding it into the card builder function
+  return fetch("http://localhost:3000/toys", obj)
+  .then(resp => resp.json())
+  .then(function(newToy) {
+    buildCard(newToy);
+  })
+  .catch(error => console.log(error.message));
 };
 
-// Render newly created toy
-
-async function displayToys() {
-  let toyCollection = document.getElementById("toy-collection");
-  const allToys = await fetchToys();
-
-  function parseToys(toys) {
-
-    function buildCard(toy){
-      const card = document.createElement("div")
-      card.setAttribute("class", "card");
-
-      let toyName = document.createElement("h2");
-      let toyImage = document.createElement("img");
-      let likes = document.createElement("p");
-      let likeBtn = document.createElement("button");
-      toyName.innerText = toy.name;
-      toyImage.setAttribute("class", "toy-avatar");
-      toyImage.src = toy.image;
-      likes.innerText = `${toy.likes} Likes`;
-      likeBtn.setAttribute("class", "like-btn");
-      likeBtn.innerText = `Like ${toy.name}`;
-      card.append(toyName, toyImage, likes, likeBtn);
-
-      toyCollection.appendChild(card);
-    };
-
-    toys.forEach(toy => { buildCard(toy) });
-  };
-
-  parseToys(allToys);
+// Build card that encapsulates toy
+function buildCard(toy){
+  const card = document.createElement("div")
+  card.setAttribute("class", "card");
+  
+  let toyName = document.createElement("h2");
+  let toyImage = document.createElement("img");
+  let likes = document.createElement("p");
+  let likeBtn = document.createElement("button");
+  toyName.innerText = toy.name;
+  toyImage.setAttribute("class", "toy-avatar");
+  toyImage.src = toy.image;
+  likes.innerText = `${toy.likes} Likes`;
+  likeBtn.setAttribute("class", "like-btn");
+  likeBtn.innerText = `Like ${toy.name}`;
+  card.append(toyName, toyImage, likes, likeBtn);
+  
+  toyCollection.appendChild(card);
 };
 
-
+// Run displayToys and formSubmit functions on load
 document.addEventListener("DOMContentLoaded", function() {
-  displayToys();
-  formSubmit();
+  renderToys();
 });
-
-
-let addToy = false
 
 document.addEventListener("DOMContentLoaded", function() {
   const addBtn = document.querySelector("#new-toy-btn")
   const toyForm = document.querySelector(".container")
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
     addToy = !addToy
     if (addToy) {
       toyForm.style.display = "block"
+      toyForm.addEventListener("submit", function(e) {
+        event.preventDefault();
+        saveNewToy(event.target);
+      })
     } else {
       toyForm.style.display = "none"
     }
